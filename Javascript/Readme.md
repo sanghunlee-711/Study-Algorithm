10da439326f5cc5697fa54e397ff1a18b12accd0
cloudlee <cloudlee@MacBook-Pro-9.local>
2020-12-06 22:01:26 +0900

    Add:12/6 programmers - 문자열을 정수로 바꾸

---
+++ Javascript/Readme.md
@@ -0,0 +1,2 @@
+leetcode.com
+easy class
---
+++ Javascript/StringToNumb.js
@@ -0,0 +1,9 @@
+//문자열을 정수로 바꾸기
+//https://programmers.co.kr/learn/courses/30/lessons/12925
+
+//Number 메서드를 그냥 사용함
+//잘못된 값이 입력되는 경우가 없다는 전제가 있기 때문
+function solution(s) {
+  var answer = Number(s);
+  return answer;
+}
