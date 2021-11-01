f7e7a7fac2bd118998f7f5021491a5f637be2d39
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-11-02 00:12:30 +0900

    [Modify] 부적절한 풀이 위치 변경

--- Lecture/Welcome/3강/3-3.binarySearch.py
+++ Lecture/Welcome/3강/3-3.binarySearch.py
@@ -54,13 +54,3 @@ def solution(L, x):
 print(solution([2, 5, 7, 9, 11], 4))
 
 
-def solution(L, x, l, u):
-    if l > u:
-        return -1
-    mid = (l + u) // 2
-    if x == L[mid]:
-        return mid
-    elif x < L[mid]:
-        return solution(L, x, l, mid-1)
-    else:
-        return solution(L, x, mid+1, u)
