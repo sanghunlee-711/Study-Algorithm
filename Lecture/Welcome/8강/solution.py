ba63a9764ba99aa337c01a452cf18be8f762eb17
sanghunlee-711 <cloudlee711@gmail.com>
2021-11-15 10:20:31 +0900

    [Add] 프로그래머스 기본강의 > 8강 solution
    - linked list popAt method

--- Lecture/Welcome/8강/solution.py
+++ Lecture/Welcome/8강/solution.py
@@ -50,20 +50,30 @@ class LinkedList:
 
 
     def popAt(self, pos):
-      if pos < 1 or pos > self.nodeCount + 1:
-        return False
-      
-      if pos == 1:
-          
+        if pos < 1 or pos > self.nodeCount: #제거할 위치의 node가 리스트에서 벗어나는 인덱스를 가질 때
+            raise IndexError 
 
-      curr = self.getAt(pos)
-      prev = self.getAt(pos-1)
-      prev.next = curr.next
-      curr.next = None
+        if pos == 1: #제거할 노드가 첫번째 노드인 경우
+            if self.nodeCount == 1: #제거할 노드가 포함 된 리스트의 노드 개수가 한 개인 경우
+                curr = self.head # 첫번째이므로 제거할 위치의 노드는 헤드랑 같음
+                self.head = None #제거했으므로 head, tail 없어짐.
+                self.tail = None
 
-      self.nodeCount -=1
-      return curr
+            else: #제거할 노드가 포함 된 리스트의 노드 개수가 한 개가 아닌 경우
+                curr = self.head # 첫번째이므로 제거할 위치의 노드는 헤드랑 같음
+                self.head = curr.next #그 다음을 가리키는 curr.next는 이제 head를 가리키게 되어야함.
+        
+        else: #첫번째 노드가 아닌 경우
+            prev = self.getAt(pos - 1) #pos의 전의 위치의 노드
+            curr = prev.next #pop작업
+            if pos == self.nodeCount: #마지막 인 경우
+                prev.next = None
+                self.tail = prev
+            else:
+                prev.next = curr.next
 
+        self.nodeCount -= 1
+        return curr.data
 
 
     def traverse(self):
