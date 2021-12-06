88a960acd85d3b363aad435d46e90d5fc3683407
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-12-06 22:42:22 +0900

    [Add] 프로그래머스 기본강의 > 18강 이진트리 - depth메서드 구현

---
+++ Lecture/Welcome/18강-이진트리/18-1.binaryTree.py
@@ -0,0 +1,46 @@
+class Node:
+  def __init__(self, item):
+    self.data = item
+    self.left = None
+    self.right = None
+  
+  def size(self):
+    l = self.left.size() if self.left else 0
+    r = self.right.size() if self.right else 0
+    return l+r+1
+  
+  def depth(self):
+      l = self.left.depth() if self.left else 0
+      r = self.right.depth() if self.right else 0
+      return l+1 if l > r  else r+1
+  
+  def inorder(self):
+    traversal = []
+    if self.left:
+      traversal += self.left.inorder()
+    tranversal.append(self.data)
+    if self.right:
+      traversal += self.right.inorder()
+    return traversal
+
+class BinaryTree:
+  def __init__(self,r):
+    self.root = r #root가 어딘지만 지정 해주면 어차피 Node에서 이미 간선(left, right)이 정해져있기 때문에 다 정의가 된다.
+
+  def size(self):
+    if self.root:
+      return self.root.size()
+    else:
+      return 0
+
+  def depth(self):
+      if self.root:
+          return self.root.depth()
+      else:
+          return 0
+  
+  def inorder(self):
+    if self.root:
+      return self.root.inorder()
+    else:
+      return []
