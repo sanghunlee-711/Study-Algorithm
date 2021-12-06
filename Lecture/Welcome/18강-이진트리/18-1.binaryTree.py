59fe951277a0ef1e2351c7688600e73a1c3de521
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-12-06 22:58:05 +0900

    [Add] 18강 > 이진트리 - 전위 순회 구현

--- Lecture/Welcome/18강-이진트리/18-1.binaryTree.py
+++ Lecture/Welcome/18강-이진트리/18-1.binaryTree.py
@@ -18,9 +18,18 @@ class Node:
     traversal = []
     if self.left:
       traversal += self.left.inorder()
-    tranversal.append(self.data)
+    traversal.append(self.data)
     if self.right:
       traversal += self.right.inorder()
+    return traversal
+  
+  def preorder(self):
+    traversal = []
+    traversal.append(self.data)
+    if self.left:
+      traversal += self.left.preorder()
+    if self.right:
+      traversal = self.right.preorder()
     return traversal
 
 class BinaryTree:
@@ -42,5 +51,11 @@ class BinaryTree:
   def inorder(self):
     if self.root:
       return self.root.inorder()
+    else:
+      return []
+
+  def preorder(self):
+    if self.root:
+      return self.root.preorder()
     else:
       return []
