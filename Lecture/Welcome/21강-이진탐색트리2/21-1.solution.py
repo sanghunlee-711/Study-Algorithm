c5c80888d7ce0b8d67a075d8ddbc5d072c167fd1
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-12-12 00:42:49 +0900

    [Add] 프로그래머스 기본강의 21강 > solution

--- Lecture/Welcome/21강-이진탐색트리2/21-1.solution.py
+++ Lecture/Welcome/21강-이진탐색트리2/21-1.solution.py
@@ -100,11 +100,76 @@ class BinSearchTree:
     else:
       self.root = Node(key, data) #없으면 이제 새로운 노드를 만들어서 루트로 만들어줌
   
-  def remove(self, key): #입력 : 키 , 출력: 삭제할 경우 True, 해당키의 node가 없는경우 False
-    node, parent = self.lookup(key)
-    if node:
-      #sth
-      return True
-    else:
-      #sth2
-      return False
+  def remove(self, key):
+      node, parent = self.lookup(key) #여기서 node가 지울애이고 parent가 그 부모노드
+      if node:
+          nChildren = node.countChildren()
+          # The simplest case of no children
+          if nChildren == 0:
+              # 만약 parent 가 있으면
+              # node 가 왼쪽 자식인지 오른쪽 자식인지 판단하여
+              # parent.left 또는 parent.right 를 None 으로 하여
+              # leaf node 였던 자식을 트리에서 끊어내어 없앱니다.
+              if parent:
+                  if parent.left == node:
+                      parent.left = None
+                  else:
+                      parent.right = None
+              # 만약 parent 가 없으면 (node 는 root 인 경우)
+              # self.root 를 None 으로 하여 빈 트리로 만듭니다.
+              else:
+                  self.root = None
+          # When the node has only one child
+          elif nChildren == 1:
+              # 하나 있는 자식이 왼쪽인지 오른쪽인지를 판단하여
+              # 그 자식을 어떤 변수가 가리키도록 합니다.
+              child = None
+              if node.left:
+                  child = node.left
+              else:
+                  child = node.right
+              # 만약 parent 가 있으면
+              # node 가 왼쪽 자식인지 오른쪽 자식인지 판단하여
+              # 위에서 가리킨 자식을 대신 node 의 자리에 넣습니다.
+              if parent:
+                  if parent.left == node:
+                      parent.left = child
+                  else:
+                      parent.right = child
+              # 만약 parent 가 없으면 (node 는 root 인 경우)
+              # self.root 에 위에서 가리킨 자식을 대신 넣습니다.
+              else:
+                  self.root = child
+          # When the node has both left and right children
+          else:
+              # parent 는 node 를 가리키고 있고,
+              # successor 는 node 의 오른쪽 자식을 가리키고 있으므로
+              parent = node
+              successor = node.right
+
+              # successor 로부터 왼쪽 자식의 링크를 반복하여 따라감으로써
+              # 순환문이 종료할 때 successor 는 바로 다음 키를 가진 노드를,
+              # 그리고 parent 는 그 노드의 부모 노드를 가리키도록 찾아냅니다.
+              # 하나 큰 키의 노드를 찾기 위해서는 삭제하려는 노드의 오른쪽 노드로 가서
+              # 제일 왼쪽 노드까지 반복하게 되면 찾을 수 있게 되는 구조가 이진탐색트리임.
+              while successor.left:
+                  parent = successor
+                  successor = successor.left
+                  
+              # 삭제하려는 노드인 node 에 successor 의 key 와 data 를 대입합니다.
+              node.key = successor.key
+              node.data = successor.data
+              # 이제, successor 가 parent 의 왼쪽 자식인지 오른쪽 자식인지를 판단하여
+              # 그에 따라 parent.left 또는 parent.right 를
+              # successor 가 가지고 있던 (없을 수도 있지만) 자식을 가리키도록 합니다.
+              if parent.left == successor:
+                  parent.left = successor.right #successor는 애초에 오른쪽에 값이 할당되어있는놈이므로
+              else:
+                  parent.right = successor.right
+
+          return True
+
+      else:
+          return False
+
+
