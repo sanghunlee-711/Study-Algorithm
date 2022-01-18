7f48e29cc1d2d7e8f10f119a08e273378d2ade88
cloudlee <cloudlee@MacBook-Pro-9.local>
2022-01-18 21:25:16 +0900

    [Add] 프로그래머스 기본강의 JS > 19강 최소 신장 트리

---
+++ Lecture/AtoZ-JS/18강-최단경로 알고리즘/minHeap.js
@@ -0,0 +1,48 @@
+
+class MinHeap {
+  constructor(){
+    this.heap = [null];
+  }
+
+  push(value){
+    this.heap.push(value);
+    let currentIndx = this.heap.length-1;
+    let parentIndex = Math.floor(currentIndx/2);
+
+    while(parentIndex !== 0 && this.heap[parentIndex] > value) {
+      const temp = this.heap[parentIndex];
+      this.heap[parentIndex] = value;
+      this.heap[currentIndx] = temp;
+
+      currentIndx = parentIndex;
+      parentIndex = Math.floor(currentIndx/2);
+    }
+  }
+
+  pop(){
+    const returnValue = this.heap[1];
+    this.heap[1] = this.heap.pop();
+
+    let currentIndex = 1;
+    let leftIndex = 2;
+    let rightIndex = 3;
+
+    while(this.heap[currentIndex] > this.heap[leftIndex] || 
+              this.heap[currentIndex] > this.heap[rightIndex]) {
+                if(this.heap[leftIndex] > this.heap[rightIndex]) {
+                  const temp = this.heap[currentIndex];
+                  this.heap[currentIndex] = value;
+                  this.heap[rightIndex] = temp;
+                  currentIndex = rightIndex;
+                }else{
+                  const temp = this.heap[currentIndex];
+                  this.heap[currentIndex] = value;
+                  this.heap[leftIndex] = temp;
+                  currentIndex = leftIndex;
+                }
+                leftIndex = currentIndex*2;
+                rightIndex = currentIndex * 2 + 1;
+              }
+              return returnValue;
+  }
+}
