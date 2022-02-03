d9ab6b35bb5fba0f535e0e4cc40bb3618f23ae9b
cloudlee <cloudlee@MacBook-Pro-9.local>
2022-02-03 17:32:29 +0900

    [Add] LeetCode > 20220203 ref

--- LeetCode/EasyCollection-TopInterviewQuestions/Math/countprimes.js
+++ LeetCode/EasyCollection-TopInterviewQuestions/Math/countprimes.js
@@ -1,3 +1,21 @@
+// ref: https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/744/
+// Given an integer n, return the number of prime numbers that are strictly less than n.
+
+// Example 1:
+// Input: n = 10
+// Output: 4
+// Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+// Example 2:
+// Input: n = 0
+// Output: 0
+
+// Example 3:
+// Input: n = 1
+// Output: 0
+
+// Constraints:
+// 0 <= n <= 5 * 106
 
 // first trail
 // 평소에 하던 방식으로 소수 찾는 함수 만들고 제곱근까지만 카운트해서 logN으로 만들기
