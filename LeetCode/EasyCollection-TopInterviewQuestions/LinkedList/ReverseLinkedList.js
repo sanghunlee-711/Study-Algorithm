fab25d7cee40fb397a5f50e0811059594ed3fcad
cloudlee <cloudlee@MacBook-Pro-9.local>
2022-02-04 17:55:21 +0900

    [Add] LeetCode > 20220204 ref

--- LeetCode/EasyCollection-TopInterviewQuestions/LinkedList/ReverseLinkedList.js
+++ LeetCode/EasyCollection-TopInterviewQuestions/LinkedList/ReverseLinkedList.js
@@ -1,3 +1,9 @@
+//ref:https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/560/
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
 
 
 // First Trail
