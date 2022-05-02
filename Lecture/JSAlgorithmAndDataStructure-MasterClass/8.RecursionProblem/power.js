3ff6c8bcd6f39e791e575edac663a7af01933e5c
cloudlee <cloudlee@MacBook-Pro-9.local>
2022-05-02 16:04:08 +0900

    [Add] Udemy > Recursion Practice : Power

---
+++ Lecture/JSAlgorithmAndDataStructure-MasterClass/8.RecursionProblem/power.js
@@ -0,0 +1,15 @@
+/*
+Write a function called power which accepts a base and an exponent.
+ The function should return the power of the base to the exponent. 
+ This function should mimic the functionality of  Math.pow()
+  - do not worry about negative bases and exponents.
+*/
+
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+function power(a, b){
+  if(b === 0) return 1;
+  return a * power(a, b - 1);
+}
